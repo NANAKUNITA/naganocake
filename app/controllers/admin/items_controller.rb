87958a004a9338cdae254b12c14d0d388c3c2442
EC2 Default User <ec2-user@ip-172-31-28-160.ap-northeast-1.compute.internal>
2023-01-18 13:45:47 +0000

    [Update]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,22 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+    @items=Item.page(params[:page]).per(10)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
