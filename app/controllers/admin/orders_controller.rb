1bb29b5261f2e5a099ac374dbbbd92d015a6204a
EC2 Default User <ec2-user@ip-172-31-44-206.ap-northeast-1.compute.internal>
2023-02-02 05:24:38 +0000

    [Update]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,11 +3,18 @@ class Admin::OrdersController < ApplicationController
         
         def show
             @order=Order.find(params[:id]) #注文内容の情報の取得
-            @order_items=@order.order_items #注文内容の商品を取得
+            @order_details=@order.order_details #注文内容の商品を取得
         end
         
         def update
+            @order=Order.find(params[:id]) #注文詳細の特定
+            @order_details=@order.order_details #注文から紐ずく商品の取得
+            @order.update(order_params) #注文ステータスの更新
             
+            if @order.status == "入金確認" #注文ステータスが入金確認であれば以下のことを実行
+            @order_details.update_all(making_status: 1) #製作ステータスを「製作待ち」へ更新
+            end
+            redirect_to admin_order_path(@order) #注文履歴詳細へ
         end
     
 end
