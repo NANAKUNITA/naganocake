3c664992a7782235ecdac363044f4b2a04cc381e
EC2 Default User <ec2-user@ip-172-31-44-206.ap-northeast-1.compute.internal>
2023-02-18 05:04:04 +0000

    [Update]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -28,9 +28,9 @@ class Admin::ItemsController < ApplicationController
   def update
     @item=Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to admin_item_path(@item.id)
+      redirect_to admin_item_path(@item)
     else
-      redirect_to edit_admin_item_path(@item)
+      render "show"
     end
   end
   
